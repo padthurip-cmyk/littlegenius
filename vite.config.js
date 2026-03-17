20e8f6453a23c80a7eeb2538acfdc251c57791fe
padthurip-cmyk <padthurip@gmail.com>
2026-03-17 19:40:58 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,13 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  worker: {
+    format: 'es',
+  },
+  optimizeDeps: {
+    exclude: ['@huggingface/transformers']
+  },
+  build: {
+    target: 'esnext',
+  }
 })
